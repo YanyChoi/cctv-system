8d76987c3fe2f30890eae87c28dced6bf15a3adb
Yany Choi <51287461+YanyChoi@users.noreply.github.com>
2023-01-01 13:09:02 +0900

    Update stream.py

--- api/stream.py
+++ api/stream.py
@@ -36,16 +36,17 @@ def writeVideo():
     
     # DIVX 코덱 적용 # 코덱 종류 # DIVX, XVID, MJPG, X264, WMV1, WMV2
     # 무료 라이선스의 이점이 있는 XVID를 사용
-    fourcc = cv2.VideoWriter_fourcc('x', '2', '6', '4')
+    fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
     
     # 비디오 저장
     # cv2.VideoWriter(저장 위치, 코덱, 프레임, (가로, 세로))
     out = cv2.VideoWriter(path, fourcc, fps, (streaming_window_width, streaming_window_height))
+    print(f'start recording {fileName}.mp4...')
     while True:
         #현재시간 가져오기
         newTime = datetime.datetime.now()
         if currentTime.hour < newTime.hour or (currentTime.hour == 23 and newTime.hour == 0):
-
+            print(f'store {fileName}.mp4')
             out.release()  # out 객체 해제
             currentTime = newTime
             #현재 시간을 '년도 달 일 시간 분 초'로 가져와서 문자열로 생성
